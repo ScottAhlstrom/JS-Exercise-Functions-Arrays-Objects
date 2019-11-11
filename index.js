39ec43307e966d5f9dbfd1bad1d289840228143f
Scott Ahlstrom <srahlstrom@gmail.com>
2019-11-11 16:14:54 -0700

    update to project

--- index.js
+++ index.js
@@ -39,8 +39,14 @@ function addNumbers(num1, num2) {
  *   email: "leia@leia.com",
  * }
 */
-function makePersonObject(/* code here */) {
-  /* code here */
+function makePersonObject(id, name, email) {
+  
+  let person = {
+    id: id, 
+    name: name, 
+    email: email
+  };
+  return person;
 }
 
 /**
@@ -56,8 +62,8 @@ function makePersonObject(/* code here */) {
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
-  /* code here */
+function getName(person) {
+  return 'Hello, my name is ' + person.name;
 }
 
 /**
@@ -73,8 +79,13 @@ function getName(/* code here */) {
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
 */
-function makeSmartPerson(/* code here */) {
-  /* code here */
+function makeSmartPerson(name) {
+  const chalSmart = {
+    name: name,
+    sum: (num1, num2) => num1 + num2,
+    speak: () => 'Hello, my name is ' + name
+  }
+  return chalSmart;
 }
 
 
